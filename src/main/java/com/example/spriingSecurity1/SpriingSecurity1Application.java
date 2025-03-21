223e12194dd0834af693468cac26b8a393f7d96c
hanjaejung <hanjja12@naver.com>
2025-03-21 16:07:21 +0900

    간단한 rest api

---
+++ src/main/java/com/example/spriingSecurity1/SpriingSecurity1Application.java
@@ -0,0 +1,14 @@
+package com.example.spriingSecurity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpriingSecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpriingSecurity1Application.class, args);
+	}
+
+}
