7766bfe2caa3c7cc6087a33f62d2bc03632240e8
hanjaejung <hanjja12@naver.com>
2025-03-21 16:36:01 +0900

    security dependency 추가

--- src/main/java/com/example/spriingSecurity1/controller/WelcomeController.java
+++ src/main/java/com/example/spriingSecurity1/controller/WelcomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/welcome") //의존성에 시큐리티만 추가해도 /welcome 접속시 로그인해야 한다
     public String welcome(){
-        return "Welcome to Spring Application with out security";
+        return "Welcome to Spring Application with security";
     }
 
 }
