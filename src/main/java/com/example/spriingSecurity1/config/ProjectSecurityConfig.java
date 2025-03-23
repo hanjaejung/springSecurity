d542c6062105fda510dda5b70a1cf4dcc3cf99e1
hanjaejung <hanjja12@naver.com>
2025-03-23 14:34:12 +0900

    userDetails 객체 생성 및 전달,  어플리케이셔 메모리내에 사용자 세부내용 저장

--- src/main/java/com/example/spriingSecurity1/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/spriingSecurity1/config/ProjectSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,8 +22,17 @@ public class ProjectSecurityConfig {
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
         http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
                 .requestMatchers("notices","contact","error").permitAll());
-        http.formLogin(flc -> flc.disable()); //httpSecurityFormLoginConfigurer
-        http.httpBasic(hbc -> hbc.disable()); //httpSecurityHttpBasicConfigurer
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("testUser").password("{noop}1111").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}2222").authorities("admin").build();
+
+        return new InMemoryUserDetailsManager(user, admin); //userDetails 객체 전달,  어플리케이셔 메모리내에 사용자 세부내용 저장
+        //여러명의 사용자 받아드릴수 있다
     }
 }
